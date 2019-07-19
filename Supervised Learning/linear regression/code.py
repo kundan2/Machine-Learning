fae3dd6239979070f846645cb9c6c7825c95763c
kundan2 <kundandalvi2@gmail.com>
2019-07-20 00:35:30 +0530

    Create code.py

---
+++ Supervised Learning/linear regression/code.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize']= (20.0, 10.0)
+
+data=pd.read_csv('F:\work\Machine-Learning\Supervised Learning\linear regression\headbrain.csv')
+print(data.shape)
+data.head()
+
+
+# In[7]:
+
+
+X = data['Head Size(cm^3)'].values
+Y = data['Brain Weight(grams)'].values
+
+
+# In[9]:
+
+
+mean_x = np.mean(X)
+mean_y = np.mean(Y)
+
+m = len(X)
+numer = 0
+denom = 0
+for i in range(m):
+    numer +=(X[i] - mean_x)*(Y[i]-mean_y)
+    denom += (X[i] - mean_x)**2
+b1 = numer / denom
+b0 = mean_y - (b1 * mean_x)
+
+print(b1, b0)
+
+
+# In[14]:
+
+
+max_x = np.max(X) + 100
+min_x = np.min(X) - 100
+
+x = np.linspace(min_x, max_x, 1000)
+y = b0 + b1 * x
+
+plt.plot(x, y, color='#58b970', label='Regression Line')
+plt.scatter(X, Y, c='#ef5423', label='Scatter plot')
+
+plt.xlabel('Head size in cm3')
+plt.ylabel('Brain Weight in grams')
+
+plt.legend()
+plt.show()
+
+
+# In[15]:
+
+
+ss_t = 0 
+ss_r = 0
+for i in range(m):
+    y_pred = b0 + b1 * X[i]
+    ss_t += (Y[i] - mean_y)**2
+    ss_r += (Y[i] - y_pred)**2
+    
+r2 = 1 - (ss_r/ss_t)
+print(r2)
+
+    
+
+
+# In[ ]:
+
+
+
+
